d7eb97c11ba7d6a88154c90a1a0b83eb5f9709e7
walterbaya <walterbaya@yahoo.com>
2022-10-09 13:17:36 -0300

    se agrega funcionamiento al router

--- client/palma-store/src/App.js
+++ client/palma-store/src/App.js
@@ -1,58 +1,54 @@
-import './App.css';
-import { Component } from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Registrar from './components/RegistrarVenta';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/registrar",
-    element: <Registrar/>,
-  },
-]);
-
-
+import "./App.css";
+import { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Registrar from "./components/RegistrarVenta";
+import Consultar from "./components/ConsultarVentas";
+import Disponibilidad from "./components/DisponibilidadArticulo";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <header>
-          <h1 className="main-title w-100 m-0">Palma Store Sistema de Gestión</h1>
+          <h1 className="main-title w-100 m-0">
+            Palma Store Sistema de Gestión
+          </h1>
         </header>
 
         <section className="d-flex flex-row">
           <div className="main-buttons-container">
             <div id="main-buttons" className="main-buttons">
-              <button className="btn-main my-1" >
+              <Link className="btn-main my-1" to={`/`}>
                 Consultar Disponibilidad de Artículo
-              </button>
-              <a className="btn-main my-1" href={`/registrar`}>
+              </Link>
+              <Link className="btn-main my-1" to="/registrar">
                 Registrar Venta
-              </a>
-              <button className="btn-main my-1">
+              </Link>
+              <Link className="btn-main my-1" to={`/consultar_ventas`}>
                 Consultar Ventas
-              </button>
+              </Link>
             </div>
           </div>
-
           <div className="explanation-text-main">
-            <RouterProvider router={router} />
+            <switch>
+              <Routes>
+                <Route path="/registrar" element=<Registrar /> />
+                <Route path="/consultar_ventas" element=<Consultar /> />
+                <Route path="/" element=<Disponibilidad /> />
+              </Routes>
+            </switch>
           </div>
         </section>
-
         <footer>
-          <p>Propiedad de Palma Store, sistema de uso personal y privado. <br /> <br />
+          <p>
+            Propiedad de Palma Store, sistema de uso personal y privado. <br />{" "}
+            <br />
             Este sitio fue diseñado y desarrollado por Walter Ariel Baya
           </p>
-          @Copyright </footer>
-      </div>
-    )
-
-      ;
+          @Copyright{" "}
+        </footer>
+      </Router>
+    );
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var cors = require('cors');
-const database_connection = require('./dao/createTable');
+var express = require("express");
+var cors = require("cors");
+const database_connection = require("./dao/createTable");
 
-database_connection.create_table()
+database_connection.create_table();
 //create connection database
 const dao = database_connection;
 
@@ -12,11 +12,33 @@ app.use(express.json());
 
 var PORT = 3000;
 
-app.post('/guardar_factura', function (req, res) {
+app.post("/guardar_factura", function (req, res) {
+  const validacion = validarFormulario(req.body);
+  if (validacion == "ok") {
     dao.create_factura(req.body);
-    res.send();
+    res.send("success");
+  } else {
+    res.send(validacion);
+  }
 });
 
 app.listen(PORT, function () {
-    console.log('Server is running on PORT:', PORT);
+  console.log("Server is running on PORT:", PORT);
 });
+
+function validarFormulario(factura) {
+  if (!factura.nombre_articulo_cliente) {
+    return "Error, se debe ingresar el nombre del articulo";
+  }
+  if (!factura.precio_venta || factura.precio_venta <= 0) {
+    return "Error, se debe ingresar el precio de venta y debe ser mayor o igual a 0";
+  }
+  if (!factura.dni_cliente) {
+    return "Error, se debe ingresar el dni del cliente";
+  }
+  if (!factura.nombre_y_apellido_cliente) {
+    return "Error, se debe ingresar el nombre y apellido del cliente";
+  }
+
+  return "ok";
+}
