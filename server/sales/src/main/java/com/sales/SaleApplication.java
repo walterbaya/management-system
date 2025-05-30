899dfaba50eca6c9b26369144b692ce953120de7
walterbaya <56239454+walterbaya@users.noreply.github.com>
2025-05-30 14:58:54 -0300

    Se realizan cambios en SaleMapper y en saveSales
    - Se efectua separation of concerns
    - Se crea una clase DateTimeConfig, la idea de fondo es que sea una clase de configuracion, una vez cargada no queremos que sea posible modificarla seteando sus valores, sino que buscamos que los mismos queden estaticos y inmutables. Es de solo lectura.
    -Se cambia a formato componente tambien el DateTimeConfig

--- server/sales/src/main/java/com/sales/SaleApplication.java
+++ server/sales/src/main/java/com/sales/SaleApplication.java
@@ -1,9 +1,12 @@
 package com.sales;
 
+import com.sales.config.DateTimeConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DateTimeConfig.class)
 public class SaleApplication {
 
 	public static void main(String[] args) {
--- server/sales/src/main/java/com/sales/mapper/SaleMapper.java
+++ server/sales/src/main/java/com/sales/mapper/SaleMapper.java
@@ -1,10 +1,19 @@
 package com.sales.mapper;
 
+import com.sales.config.DateTimeConfig;
 import com.sales.dto.SaleDto;
 import com.sales.model.Sale;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.time.ZonedDateTime;
 
+@AllArgsConstructor
+@Component
 public class SaleMapper {
-    public static SaleDto toDto(Sale sale) {
+
+    private final DateTimeConfig dateTimeConfig;
+
+    public SaleDto toDto(Sale sale) {
         SaleDto saleDto = new SaleDto();
         // Convertir Sale a SaleDto
         saleDto.setColor(sale.getColor());
@@ -19,11 +28,12 @@ public class SaleMapper {
         return saleDto; // Implementa la conversión de Sale a SaleDto
     }
 
-    public static Sale toEntity(SaleDto saleDto) {
+    public Sale toEntity(SaleDto saleDto, ZonedDateTime jdbcTime) {
+
         Sale sale = new Sale();
         // Convertir SaleDto a Sale
         sale.setColor(saleDto.getColor());
-        sale.setEmissionDate(saleDto.getEmissionDate());
+        sale.setEmissionDate(jdbcTime);
         sale.setClientDni(saleDto.getClientDni());
         sale.setClientNameAndSurname(saleDto.getClientNameAndSurname());
         sale.setGender(saleDto.getGender());
